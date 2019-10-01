177a17998c91e1cfcdf04c47b19ff88bce970b62
DankoMaksym <dankomaksym@gmail.com>
2019-10-01 19:40:06 +0300

    Basic design finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'about/index'
-    get '/portfolio' => 'portfolio#index'
 	root 'pages#welcome'
+	get '/about' => 'about#index'
+	get '/contact' => 'pages#contact'
+	get '/portfolio' => 'portfolio#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
